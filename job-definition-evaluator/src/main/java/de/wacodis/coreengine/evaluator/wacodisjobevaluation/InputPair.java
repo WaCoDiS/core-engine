5a9674925199594770a212029f95ee583afdfe9f
arnevogt <arne.vogt@hs-bochum.de>
2018-11-26 17:42:54 +0100

    add Unit Tests for InputPair

--- job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/wacodisjobevaluation/InputPair.java
+++ job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/wacodisjobevaluation/InputPair.java
@@ -9,7 +9,7 @@ import de.wacodis.core.models.AbstractResource;
 import de.wacodis.core.models.AbstractSubsetDefinition;
 
 /**
- *
+ * key-value pairs of SubsetDefinition (key) and Resource (value)
  * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
  */
 public class InputPair {
@@ -22,6 +22,10 @@ public class InputPair {
         this.resource = resource;
     }
     
+    /**
+     * creates instance with resource set to null
+     * @param subsetDefinition 
+     */
     public InputPair(AbstractSubsetDefinition subsetDefinition){
         this.subsetDefinition = subsetDefinition;
         this.resource = null;
@@ -39,22 +43,42 @@ public class InputPair {
         return subsetDefinition;
     }
     
+    /**
+     * alias for getSubsetDefinition
+     * @return 
+     */
     public AbstractSubsetDefinition getKey(){
         return getSubsetDefinition();
     }
     
+    /**
+     * alias for getResource
+     * @return 
+     */
     public AbstractResource getValue(){
         return getResource();
     }
     
+    /**
+     * alias for setResource
+     * @param resource 
+     */
     public void setValue(AbstractResource resource){
         setResource(resource);
     }
     
+    /**
+     * alias for hasResource
+     * @return 
+     */
     public boolean hasValue(){
         return resource != null;
     }
     
+    /**
+     * returns true if resource is not null
+     * @return 
+     */
     public boolean hasResource(){
         return hasValue();
     }
