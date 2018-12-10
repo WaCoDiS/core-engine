e07cded4eedb8a376ccdcfa685bf9ed23b1cf197
arnevogt <arne.vogt@hs-bochum.de>
2018-12-10 16:04:10 +0100

    add DataAccessConnector

---
+++ job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/http/dataaccess/DataAccessConnector.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.evaluator.http.dataaccess;
+
+import de.wacodis.core.models.AbstractResource;
+import de.wacodis.core.models.DataAccessResourceSearchBody;
+import de.wacodis.coreengine.evaluator.http.DataAccessResourceSearchRequest;
+import de.wacodis.coreengine.evaluator.http.GenericPostRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+
+/**
+ *
+ * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
+ */
+public class DataAccessConnector implements ResourceProvider<Map<String, List<AbstractResource>>, DataAccessResourceSearchBody> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataAccessConnector.class);
+    
+    private final GenericPostRequest<DataAccessResourceSearchBody, Map<String, List<AbstractResource>>> dataAccessRequest;
+
+    public DataAccessConnector() {
+        this.dataAccessRequest = new DataAccessResourceSearchRequest();
+    }
+
+    public DataAccessConnector(GenericPostRequest<DataAccessResourceSearchBody, Map<String, List<AbstractResource>>> dataAccessRequest) {
+        this.dataAccessRequest = dataAccessRequest;
+    }
+
+    public DataAccessConnector(URL url) {
+        this();
+        this.dataAccessRequest.setUrl(url);
+    }
+
+    public DataAccessConnector(URL url, GenericPostRequest<DataAccessResourceSearchBody, Map<String, List<AbstractResource>>> dataAccessRequest) {
+        this(dataAccessRequest);
+        this.dataAccessRequest.setUrl(url);
+    }
+
+    public URL getUrl() {
+        return this.dataAccessRequest.getUrl();
+    }
+
+    public void setUrl(URL url) {
+        this.dataAccessRequest.setUrl(url);
+    }
+
+    /**
+     *
+     * @param searchBody
+     * @return
+     * @throws java.io.IOException
+     */
+    @Override
+    public Map<String, List<AbstractResource>> searchResources(DataAccessResourceSearchBody searchBody) throws IOException {
+        this.dataAccessRequest.setPayload(searchBody);
+        ResponseEntity<Map<String, List<AbstractResource>>> response = this.dataAccessRequest.execute();
+        
+        if(response.getStatusCode().is2xxSuccessful() && response.hasBody()){
+            return response.getBody();
+        }else{
+            LOGGER.error("Retrieving data from data-acces-api failed, Url: " + this.dataAccessRequest.getUrl().toString() +
+                         " StatusCode: " + response.getStatusCodeValue() + " RequestBody: " + searchBody.toString() 
+                         + " hasResponseBody: " + response.hasBody());
+            throw new java.io.IOException("Could not retrieve resources from " + this.dataAccessRequest.getUrl().toString()+ ", StatusCode: " + response.getStatusCodeValue() + ", hasBody: " + response.hasBody());
+        }
+    }
+
+}
---
+++ job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/http/dataaccess/ResourceProvider.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.evaluator.http.dataaccess;
+
+/**
+ *
+ * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
+ * @param <R> response
+ * @param <Q> query
+ */
+public interface ResourceProvider<R,Q> {
+    
+    R searchResources(Q query) throws java.io.IOException;
+    
+}
