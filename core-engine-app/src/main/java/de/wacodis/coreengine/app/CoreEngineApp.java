eef7757e46f689097c6f5932ec58de5ab924df68
arnevogt <arne.vogt@hs-bochum.de>
2018-12-19 16:29:55 +0100

    enable dynamic configuration

--- core-engine-app/src/main/java/de/wacodis/coreengine/app/CoreEngineApp.java
+++ core-engine-app/src/main/java/de/wacodis/coreengine/app/CoreEngineApp.java
@@ -5,10 +5,13 @@
  */
 package de.wacodis.coreengine.app;
 
+import de.wacodis.coreengine.evaluator.configuration.DataEnvelopeMatchingConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -17,11 +20,16 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan({
+    "de.wacodis.coreengine.app",
     "de.wacodis.coreengine.evaluator",
     "de.wacodis.coreengine.scheduling"})
+@RefreshScope
 public class CoreEngineApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CoreEngineApp.class);
+
+    @Autowired
+    private DataEnvelopeMatchingConfiguration dataMatchingConfiguration;
 
     /**
      * @param args the command line arguments
---
+++ job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/configuration/DataEnvelopeMatchingConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.evaluator.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
+ */
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("spring.evaluator.matching")
+public class DataEnvelopeMatchingConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataEnvelopeMatchingConfiguration.class);
+    
+    private float minimumOverlapPercentage;
+
+    public float getMinimumOverlapPercentage() {
+        return minimumOverlapPercentage;
+    }
+
+    public void setMinimumOverlapPercentage(float minimumOverlapPercentage) {
+        this.minimumOverlapPercentage = minimumOverlapPercentage;
+        LOGGER.info("minimumOverlapPercentage set to: " + this.minimumOverlapPercentage);
+    }
+}
