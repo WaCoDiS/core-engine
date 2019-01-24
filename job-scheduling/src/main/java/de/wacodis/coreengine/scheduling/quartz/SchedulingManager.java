2b7963ad966ddd94b99970495f5bbc542b508b00
SebaDro <sebastian.drost@hs-bochum.de>
2019-01-24 07:54:21 +0100

    Adjust exception handling by
    Exceptions will be handled inside the scheduling manager rather than being thrown

--- job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/quartz/SchedulingManager.java
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/quartz/SchedulingManager.java
@@ -8,9 +8,12 @@ package de.wacodis.coreengine.scheduling.quartz;
 import de.wacodis.core.models.WacodisJobDefinition;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.logging.Level;
 import org.quartz.JobDetail;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,24 +24,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchedulingManager {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchedulingManager.class);
+
     @Autowired
     private QuartzScheduler quartzScheduler;
 
     @Autowired
     private JobContextFactory jCFactory;
 
-    public Date scheduleNewJob(WacodisJobDefinition jobDefinition) throws SchedulerException, ParseException {
-        JobContext jobContext = jCFactory.createJobContext(jobDefinition);
-        return scheduleNewJob(jobContext.getJobDetails(), jobContext.getTrigger());
+    public void scheduleNewJob(WacodisJobDefinition jobDefinition) {
+        try {
+            JobContext jobContext = jCFactory.createJobContext(jobDefinition);
+            scheduleNewJob(jobContext.getJobDetails(), jobContext.getTrigger());
+        } catch (ParseException ex) {
+            LOGGER.error(ex.getMessage());
+            LOGGER.debug("Error while parsing job definition", ex);
+        }
     }
 
-    public Date scheduleNewJob(WacodisJobDefinition jobDefinition, String timeZoneId) throws SchedulerException, ParseException {
-        JobContext jobContext = jCFactory.createJobContext(jobDefinition, timeZoneId);
-        return scheduleNewJob(jobContext.getJobDetails(), jobContext.getTrigger());
+    public void scheduleNewJob(WacodisJobDefinition jobDefinition, String timeZoneId) {
+        try {
+            JobContext jobContext = jCFactory.createJobContext(jobDefinition, timeZoneId);
+            scheduleNewJob(jobContext.getJobDetails(), jobContext.getTrigger());
+        } catch (ParseException ex) {
+            LOGGER.error(ex.getMessage());
+            LOGGER.debug("Error while parsing job definition", ex);
+        }
     }
 
-    public Date scheduleNewJob(JobDetail jobDetail, Trigger trigger) throws SchedulerException {
-        return quartzScheduler.getScheduler().scheduleJob(jobDetail, trigger);	//runs QuartzJob's execute()
+    public void scheduleNewJob(JobDetail jobDetail, Trigger trigger) {
+        try {
+            Date date = quartzScheduler.getScheduler().scheduleJob(jobDetail, trigger);	//runs QuartzJob's execute()
+            LOGGER.info("Scheduling for job {} was successful. First fire time: {}", jobDetail.getKey(), date);
+        } catch (SchedulerException ex) {
+            LOGGER.error(ex.getMessage());
+            LOGGER.debug("Error while trying to schedule job", ex);
+        }
     }
 
 }
