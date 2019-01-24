e0fc80ca82fbd581fc7453812c9124110e4ae007
SebaDro <sebastian.drost@hs-bochum.de>
2019-01-24 20:32:22 +0100

    Add Spring bean factories to enable autowiring support for Quartz beans

---
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/factory/JobDetailFactory.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.scheduling.factory;
+
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Factory class for creating bean-style Quartz {@Link JobDetail} instances
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Component
+public class JobDetailFactory {
+
+    @Autowired
+    private ApplicationContext context;
+
+    /**
+     * Creates a Quartz {@Link JobDetail} that is configured by
+     * {@Link JobDetailFactoryBean}
+     *
+     * @param jobClass class that implements the job execution
+     * @param isDurable durability after job completion
+     * @param isRecoverable recover flag for the Job
+     * @param jobName job name
+     * @param jobGroup job group
+     * @param jobDataMap additional job data
+     * @return a Quartz {@Link JobDetail} instance
+     */
+    public JobDetail createJob(Class<? extends QuartzJobBean> jobClass, boolean isDurable, boolean isRecoverable,
+            String jobName, String jobGroup, JobDataMap jobDataMap) {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(jobClass);
+        factoryBean.setDurability(isDurable);
+        factoryBean.setRequestsRecovery(isRecoverable);
+        factoryBean.setApplicationContext(context);
+        factoryBean.setName(jobName);
+        factoryBean.setGroup(jobGroup);
+        factoryBean.setJobDataMap(jobDataMap);
+
+        factoryBean.afterPropertiesSet();
+
+        return factoryBean.getObject();
+    }
+
+}
---
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/factory/JobFactory.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.scheduling.factory;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+/**
+ * Helper class to enable autowiring for job beans.
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class JobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+
+}
