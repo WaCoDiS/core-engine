d6969f36da0f836313268c6261c261765371f265
SebaDro <sebastian.drost@hs-bochum.de>
2019-01-24 20:30:39 +0100

    Add configuration for Quartz scheduler

---
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/config/SchedulerConfig.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.scheduling.config;
+
+import de.wacodis.coreengine.scheduling.factory.JobFactory;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * Configuration for the Quartz scheduler
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Configuration
+public class SchedulerConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private QuartzProperties quartzProperties;
+
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * Creates a configured scheduler factory
+     *
+     * @return a configured {@link SchedulerFactoryBean} instance
+     */
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+
+        JobFactory jobFactory = new JobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setOverwriteExistingJobs(true);
+        factory.setDataSource(dataSource);
+        factory.setQuartzProperties(properties);
+        factory.setJobFactory(jobFactory);
+        return factory;
+    }
+
+}
