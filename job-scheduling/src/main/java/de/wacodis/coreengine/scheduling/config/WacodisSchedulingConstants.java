e31cd4b162636d088a40c0a9f0ceb067f2b81a92
SebaDro <sebastian.drost@hs-bochum.de>
2019-01-30 13:03:37 +0100

    Apply fix for a48eb00 by adding missing classes

---
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/config/WacodisSchedulingConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.coreengine.scheduling.config;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class WacodisSchedulingConstants {
+
+    public static final String GROUP_NAME = "de.hsbo.wacodis";
+
+    public static final String JOB_KEY_ID = "keyId";
+    
+    public static final String DEFAULT_TIMEZONE = "Europe/Berlin";
+
+}
