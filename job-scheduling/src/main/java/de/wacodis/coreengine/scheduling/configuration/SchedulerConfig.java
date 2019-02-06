6083599a9a9c4ea063c338e330941c6986274f9c
SebaDro <sebastian.drost@hs-bochum.de>
2019-02-06 16:03:05 +0100

    Rename package

--- job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/configuration/SchedulerConfig.java
+++ job-scheduling/src/main/java/de/wacodis/coreengine/scheduling/configuration/SchedulerConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.wacodis.coreengine.scheduling.config;
+package de.wacodis.coreengine.scheduling.configuration;
 
 import de.wacodis.coreengine.scheduling.job.JobFactory;
 import java.util.Properties;
