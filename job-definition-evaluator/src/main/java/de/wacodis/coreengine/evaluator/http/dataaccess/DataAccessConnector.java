86debed71afa7831b2e3f7a3f7d4a2d548477dc4
arnevogt <arne.vogt@hs-bochum.de>
2019-06-03 11:27:44 +0200

    adjust exception handling for data access calls

--- job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/http/dataaccess/DataAccessConnector.java
+++ job-definition-evaluator/src/main/java/de/wacodis/coreengine/evaluator/http/dataaccess/DataAccessConnector.java
@@ -15,15 +15,18 @@ import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 
 /**
  * search data-access-api for available resources
+ *
  * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
  */
 public class DataAccessConnector implements DataAccessResourceProvider {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataAccessConnector.class);
-    
+
     private final GenericPostRequest<DataAccessResourceSearchBody, Map<String, List<AbstractResource>>> dataAccessRequest;
 
     public DataAccessConnector() {
@@ -54,26 +57,34 @@ public class DataAccessConnector implements DataAccessResourceProvider {
 
     /**
      * execute HTTP-Post to data-access-api, instance variable Url must be set
+     *
      * @param searchBody
      * @return response body
-     * @throws java.io.IOException if request is not successful or has no response body
+     * @throws java.io.IOException if request is not successful or has no
+     * response body
      */
     @Override
     public Map<String, List<AbstractResource>> searchResources(DataAccessResourceSearchBody searchBody) throws IOException {
         this.dataAccessRequest.setPayload(searchBody);
-        ResponseEntity<Map<String, List<AbstractResource>>> response = this.dataAccessRequest.execute();
-        
-        if(response.getStatusCode().is2xxSuccessful() && response.hasBody()){
-            LOGGER.info("Retrieving data from data-acces-api succeeded, Url: " + this.dataAccessRequest.getUrl().toString() +
-                         ", StatusCode: " + response.getStatusCodeValue() + ", RequestBody: " + searchBody.toString() 
-                         + ", ResponseBody: " + response.getBody().toString());
-            
-            return response.getBody();
-        }else{
-            LOGGER.error("Retrieving data from data-acces-api failed, Url: " + this.dataAccessRequest.getUrl().toString() +
-                         ", StatusCode: " + response.getStatusCodeValue() + ", RequestBody: " + searchBody.toString() 
-                         + ", hasResponseBody: " + response.hasBody());
-            throw new java.io.IOException("Could not retrieve resources from " + this.dataAccessRequest.getUrl().toString()+ ", StatusCode: " + response.getStatusCodeValue() + ", hasBody: " + response.hasBody());
+
+        try {
+            ResponseEntity<Map<String, List<AbstractResource>>> response = this.dataAccessRequest.execute();
+            if (response.hasBody()) {
+                LOGGER.info("Retrieving data from data-acces-api succeeded, Url: " + this.dataAccessRequest.getUrl().toString()
+                        + ", StatusCode: " + response.getStatusCodeValue() + ", RequestBody: " + searchBody.toString()
+                        + ", ResponseBody: " + response.getBody().toString());
+
+                return response.getBody();
+            } else {
+                LOGGER.error("Retrieving data from data-acces-api ( " + this.dataAccessRequest.getUrl().toString() + ")failed because response has no body"
+                        + ", StatusCode: " + response.getStatusCodeValue() + ", RequestBody: " + searchBody.toString()
+                        + ", hasResponseBody: " + response.hasBody());
+                throw new java.io.IOException("Could not retrieve resources from " + this.dataAccessRequest.getUrl().toString() + ", StatusCode: " + response.getStatusCodeValue() + ", hasBody: " + response.hasBody());
+            }
+        } catch (HttpStatusCodeException ex) {
+            throw new java.io.IOException("Unable to retrieve resources from DataAccess (" + this.dataAccessRequest.getUrl().toString() + "), status code: " + ex.getStatusCode().toString() + System.lineSeparator() + "search body: " + searchBody.toString(), ex);
+        } catch (RestClientException ex) {
+            throw new java.io.IOException("Could not retrieve resources from DataAccess (" + this.dataAccessRequest.getUrl().toString() + ") due to unexpected RestClientException", ex);
         }
     }
 
