6d7089b402050e04d907caea701942a59db29763
Arne Vogt <arne.vogt@hs-bochum.de>
2019-11-08 16:53:07 +0100

    edit example

--- job-definition-executor/src/test/java/de/wacodis/coreengine/executor/process/wps/RunWPSProcessIT.java
+++ job-definition-executor/src/test/java/de/wacodis/coreengine/executor/process/wps/RunWPSProcessIT.java
@@ -9,6 +9,7 @@ import de.wacodis.coreengine.executor.process.ProcessContext;
 import org.n52.geoprocessing.wps.client.WPSClientSession;
 import de.wacodis.core.models.AbstractResource;
 import de.wacodis.core.models.GetResource;
+import de.wacodis.core.models.extension.staticresource.StaticDummyResource;
 import de.wacodis.coreengine.executor.exception.ExecutionException;
 import de.wacodis.coreengine.executor.process.ExpectedProcessOutput;
 import de.wacodis.coreengine.executor.process.ProcessOutputDescription;
@@ -43,34 +44,37 @@ public class RunWPSProcessIT {
     }
 
     private ProcessContext buildInputContext() {
-        AbstractResource opticalImgType = new GetResource();
+        StaticDummyResource opticalImgType = new StaticDummyResource();
         opticalImgType.setMethod(AbstractResource.MethodEnum.GETRESOURCE);
-        opticalImgType.setUrl("Aerial_Image");
+        opticalImgType.setValue("Sentinel-2");
 
         AbstractResource opticalImgSrc = new GetResource();
         opticalImgSrc.setMethod(AbstractResource.MethodEnum.GETRESOURCE);
-        opticalImgSrc.setUrl("http://www.example.com");
+        opticalImgSrc.setUrl("https://scihub.copernicus.eu/dhus/odata/v1/Products('21eb3657-9a68-46cf-979a-9b731dfedf24')/$value");
 
+        /*
         AbstractResource opticalImgSrc2 = new GetResource();
         opticalImgSrc2.setMethod(AbstractResource.MethodEnum.GETRESOURCE);
-        opticalImgSrc2.setUrl("http://www.beispiel.de");
+        opticalImgSrc2.setUrl("https://scihub.copernicus.eu/dhus/odata/v1/Products('72bdd44c-bc96-406f-95ee-e6e903a59452')/$value");
+        */
 
         AbstractResource refDataType = new GetResource();
         refDataType.setMethod(AbstractResource.MethodEnum.GETRESOURCE);
-        refDataType.setUrl("MANUAL");
+        refDataType.setUrl("ATKIS");
 
         AbstractResource refData = new GetResource();
         refData.setMethod(AbstractResource.MethodEnum.GETRESOURCE);
-        refData.setUrl("http://geoprocessing.demo.52north.org:8080/geoserver/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=topp:tasmania_roads&SRS=EPSG:4326&OUTPUTFORMAT=GML3");
+        refData.setUrl("http://geoprocessing.demo.52north.org:8080/geoserver/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=topp:tasmania_roads&SRS=EPSG:32632&OUTPUTFORMAT=GML3");
 
         ProcessContext inputContext = new ProcessContext();
         inputContext.addInputResource("OPTICAL_IMAGES_TYPE", new ResourceDescription(opticalImgType, "text/xml"));
         inputContext.addInputResource("OPTICAL_IMAGES_SOURCES", new ResourceDescription(opticalImgSrc, "text/xml"));
-        inputContext.addInputResource("OPTICAL_IMAGES_SOURCES", new ResourceDescription(opticalImgSrc2, "text/xml"));
+        //inputContext.addInputResource("OPTICAL_IMAGES_SOURCES", new ResourceDescription(opticalImgSrc2, "text/xml"));
         inputContext.addInputResource("REFERENCE_DATA_TYPE", new ResourceDescription(refDataType, "text/xml"));
         inputContext.addInputResource("REFERENCE_DATA", new ResourceDescription(refData, "text/xml"));
 
         inputContext.addExpectedOutput(new ExpectedProcessOutput("PRODUCT", "image/geotiff"));
+        inputContext.addExpectedOutput(new ExpectedProcessOutput("METADATA", "text/json"));
 
         inputContext.setWacodisProcessID("dummyLandCoverClassificationProcess");
 
