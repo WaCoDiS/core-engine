33e407c61dcd48d30203590766266e38578729e0
Arne Vogt <arne.vogt@hs-bochum.de>
2020-05-13 18:07:56 +0200

    build logging statements for complex inputs correctly

--- job-definition-executor/src/main/java/de/wacodis/coreengine/executor/process/wps/ToStringHelper.java
+++ job-definition-executor/src/main/java/de/wacodis/coreengine/executor/process/wps/ToStringHelper.java
@@ -6,22 +6,38 @@ import org.n52.geoprocessing.wps.client.model.execution.ExecuteOutput;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.n52.geoprocessing.wps.client.model.execution.ComplexData;
 
 public class ToStringHelper {
 
     public static String executeToString(Execute exec) {
-        return "Execute{" +
-                "inputs=[" + inputsToString(exec.getInputs()) + ']' +
-                ", outputs=[" + outputsToString(exec.getOutputs()) + ']' +
-                ", id='" + exec.getId() + '\'' +
-                '}';
+        return "Execute{"
+                + "inputs=[" + inputsToString(exec.getInputs()) + ']'
+                + ", outputs=[" + outputsToString(exec.getOutputs()) + ']'
+                + ", id='" + exec.getId() + '\''
+                + '}';
     }
 
     public static String inputsToString(List<Data> inputs) {
-        return inputs.stream().map(d -> String.format("id=%s, value=%s", d.getId(), d.getValue())).collect(Collectors.joining(", "));
+        return inputs.stream().map(d -> String.format("id=%s, value=%s", d.getId(), getInputValue(d))).collect(Collectors.joining(", "));
     }
 
     public static String outputsToString(List<ExecuteOutput> outputs) {
         return outputs.stream().map(d -> String.format("id=%s, mode=%s", d.getId(), d.getTransmissionMode())).collect(Collectors.joining(", "));
+    }
+
+    private static Object getInputValue(Data input) {
+        if (input instanceof ComplexData) {
+            ComplexData complexInput = (ComplexData) input;
+            if (complexInput.isReference() && complexInput.getReference() != null) {
+                return complexInput.getReference().getHref();
+            }
+        } else {
+            if (input != null) {
+                return input.getValue();
+            }
+        }
+        
+        return "unparseable input value";
     }
 }
